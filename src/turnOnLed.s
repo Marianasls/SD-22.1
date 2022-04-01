48137dca4f481ffd608e501099788b548fcb38aa
marianasls <marianalima0803@gmail.com>
2022-04-01 16:12:40 -0300

    teste

--- src/turnOnLed.s
+++ src/turnOnLed.s
@@ -88,14 +88,13 @@ main:
 
         @ colocando o pino 6 no modo output
         mov       r0, gpio        @ address of GPIO
-        mov       r1, 6           @ pin 6
-        mov       r2, 1           @ set pin 6 to output, 001 = 1 = output
-        bl gpioPinFSelect
-
-        @ setando o pino 6 com o valor 1
-        mov       r0, gpio        @ address of GPIO
-        mov       r1, 6           @ pin 6
-        bl gpioPinSet
+        mov       r1, 1           @ set pin 6 to output, 001 = 1 = output
+        lsl       r1, #18
+        str       r1, [r0, #0]
+        
+        mov       r2, 1
+        lsl       r2, #6
+        str       r2, [r0, #40]
 
 @-----------------------------------------------------------------------------
 @ talvez o correto seja usar o munmap entre cada chamada de função, eu não sei mas vou supor que não vai ser necessario
@@ -129,131 +128,3 @@ memMsgAddr:
         .word   memMsg
 gpio:
         .word   PERIPH+GPIO_OFFSET
-
-
-@ gpioPinFSelect.s
-@ Selects a function for a GPIO pin. Assumes that GPIO registers
-@ have been mapped to programming memory.
-@ Calling sequence:
-@       r0 <- address of GPIO in mapped memory
-@       r1 <- pin number
-@       r2 <- pin function
-@       bl gpioPinFSelect
-@ 2017-09-30: Bob Plantz
-
-@ Define my Raspberry Pi
-        @ .cpu    cortex-a53
-        @ .fpu    neon-fp-armv8
-        @ .syntax unified         @ modern syntax
-
-@ Constants for assembler
-        .equ    PIN_FIELD,0b111 @ 3 bits
-
-@ The program
-        @ .text
-        @ .align  2
-        @ .global gpioPinFSelect
-        @ .type   gpioPinFSelect, %function
-gpioPinFSelect:
-        sub     sp, sp, 24      @ space for saving regs
-                                @ (keeping 8-byte sp align)
-        str     r4, [sp, 4]     @ save r4
-        str     r5, [sp, 8]     @      r5
-        str     r6, [sp,12]     @      r6
-        str     fp, [sp, 16]    @      fp
-        str     lr, [sp, 20]    @      lr
-        add     fp, sp, 20      @ set our frame pointer
-        
-        mov     r4, r0          @ save pointer to GPIO
-        mov     r5, r1          @ save pin number
-        mov     r6, r2          @ save function code
-        
-@ Compute address of GPFSEL register and pin field        
-        mov     r3, 10          @ divisor
-        udiv    r0, r5, r3      @ GPFSEL number
-        
-        mul     r1, r0, r3      @ compute remainder
-        sub     r1, r5, r1      @     for GPFSEL pin
-        
-@ Set up the GPIO pin funtion register in programming memory
-        lsl     r0, r0, 2       @ 4 bytes in a register
-        add     r0, r4, r0      @ GPFSELn address
-        ldr     r2, [r0]        @ get entire register
-        
-        mov     r3, r1          @ need to multiply pin
-        add     r1, r1, r3, lsl 1   @    position by 3
-        mov     r3, PIN_FIELD   @ gpio pin field
-        lsl     r3, r3, r1      @ shift to pin position
-        bic     r2, r2, r3      @ clear pin field
-
-        lsl     r6, r6, r1      @ shift function code to pin position
-        orr     r2, r2, r6      @ enter function code
-        str     r2, [r0]        @ update register
-        
-        mov     r0, 0           @ return 0;
-        ldr     r4, [sp, 4]     @ restore r4
-        ldr     r5, [sp, 8]     @      r5
-        ldr     r6, [sp,12]     @      r6
-        ldr     fp, [sp, 16]    @      fp
-        ldr     lr, [sp, 20]    @      lr
-        add     sp, sp, 24      @      sp
-        bx      lr              @ return
-
-
-        @ gpioPinSet.s
-@ Sets a GPIO pin. Assumes that GPIO registers
-@ have been mapped to programming memory.
-@ Calling sequence:
-@       r0 <- address of GPIO in mapped memory
-@       r1 <- pin number
-@       bl gpioPinSet
-@ 2017-09-30: Bob Plantz
-
-@ @ Define my Raspberry Pi
-@         .cpu    cortex-a53
-@         .fpu    neon-fp-armv8
-@         .syntax unified         @ modern syntax
-
-@ Constants for assembler
-        .equ    PIN,1           @ 1 bit for pin
-        .equ    PINS_IN_REG,32
-        .equ    GPSET0,0x1c     @ set register offset
-
-@ The program
-        @ .text
-        @ .align  2
-        @ .global gpioPinSet
-        @ .type   gpioPinSet, %function
-gpioPinSet:
-        sub     sp, sp, 16      @ space for saving regs
-        str     r4, [sp, 0]     @ save r4
-        str     r5, [sp, 4]     @      r5
-        str     fp, [sp, 8]     @      fp
-        str     lr, [sp, 12]    @      lr
-        add     fp, sp, 12      @ set our frame pointer
-        
-        add     r4, r0, GPSET0  @ pointer to GPSET regs.
-        mov     r5, r1          @ save pin number
-        
-@ Compute address of GPSET register and pin field        
-        mov     r3, PINS_IN_REG @ divisor
-        udiv    r0, r5, r3      @ GPSET number
-        mul     r1, r0, r3      @ compute remainder
-        sub     r1, r5, r1      @     for relative pin position
-        lsl     r0, r0, 2       @ 4 bytes in a register
-        add     r0, r0, r4      @ address of GPSETn
-        
-@ Set up the GPIO pin funtion register in programming memory
-        ldr     r2, [r0]        @ get entire register
-        mov     r3, PIN         @ one pin
-        lsl     r3, r3, r1      @ shift to pin position
-        orr     r2, r2, r3      @ set bit
-        str     r2, [r0]        @ update register
-        
-        mov     r0, 0           @ return 0;
-        ldr     r4, [sp, 0]     @ restore r4
-        ldr     r5, [sp, 4]     @      r5
-        ldr     fp, [sp, 8]     @         fp
-        ldr     lr, [sp, 12]    @         lr
-        add     sp, sp, 16      @ restore sp
-        bx      lr              @ return
