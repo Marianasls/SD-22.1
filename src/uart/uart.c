dd41bab537f795d1177260f4ee6b23a17e68b3cc
Esther Araújo <esthersss09@gmail.com>
2022-05-11 15:18:03 -0300

    Create uart.c

---
+++ src/uart/uart.c
@@ -0,0 +1,40 @@
+ /*
+    UART communication on Raspberry Pi using C (WiringPi Library)
+    http://www.electronicwings.com
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+int main ()
+{
+  int serial_port ;
+  char dat;
+  if ((serial_port = serialOpen("/dev/ttyAMA0", 9600)) < 0)    /* open serial port */
+  {
+    fprintf (stderr, "Unable to open serial device: %s\n", strerror(errno)) ;
+    return 1 ;
+  }
+
+  if (wiringPiSetup () == -1)                    /* initializes wiringPi setup */
+  {
+    fprintf (stdout, "Unable to start wiringPi: %s\n", strerror(errno)) ;
+    return 1 ;
+  }
+
+  while(1){
+      
+    if(serialDataAvail (serial_port) )
+    { 
+        dat = serialGetchar (serial_port);        /* receive character serially*/    
+        printf ("%c", dat) ;
+        fflush (stdout) ;
+        serialPutchar(serial_port, dat);        /* transmit character serially on port */
+          }
+    }
+
+}
