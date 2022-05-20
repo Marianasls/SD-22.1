3c808c1c018c868ecf524b1faa4d92938fded3ca
marianasls <marianalima0803@gmail.com>
2022-05-20 09:39:45 -0300

    update uart raspberry

--- src/uart/uart.c
+++ src/uart/uart.c
@@ -4,12 +4,43 @@
 
 #include <wiringPi.h>
 #include <wiringSerial.h>
+#include <termios.h>
 
-int main()
-{
+struct termios options ;
+void init_wiringSerial(int fd) {
+  tcgetattr (fd, &options) ;   // Read current options
+  // enable receiver, set 8 bit data, ignore control lines   
+  options.c_cflag |= (CLOCAL | CREAD | CS8);    
+  // disable parity generation and 2 stop bits   
+  options.c_cflag &= ~(PARENB | CSTOPB);  
+  // set the new port options   
+  tcsetattr(fd, TCSANOW, &options);      
+  return fd; 
+}
+/**
+ * Leitura serial de inteiro
+ * @param fd 
+ * @return int 
+ */
+int read_serial_int (int fd) {     
+  char ascii_int[8] = {0};     
+  char c = NULL;     
+  int i = 0;     
+
+  read(fd, &c, 1);         
+  //while (c != '\n')     
+  while (c != ' ')  {         
+    ascii_int[i++] = c;         
+    read(fd, &c, 1);     
+  }     
+  return atoi(ascii_int); 
+} 
+
+int main() {
   int serial_port;
-  int req, code_req;
+  int req, code_req, sensor_addr;
   char dat;
+  int integral_part, decimal_part;
   printf("-------------------------------------\n");
   printf("-----------------MENU----------------\n");
   printf("-------------------------------------\n");
@@ -17,6 +48,8 @@ int main()
   printf("2 - Temperatura.\n");
   printf("3 - Umidade.\n");
   scanf("%d", &req);
+  printf("Endereço do sensor: (De 0 a 31)");
+  scanf("%d", &sensor_addr);
 
   switch (req)
   {
@@ -38,22 +71,33 @@ int main()
     fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
     return 1;
   }
+  // controle de porta serial
+  init_wiringSerial(serial_port);
 
   if (wiringPiSetup() == -1) /* initializes wiringPi setup */
   {
     fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));
     return 1;
   }
+  // comando de requisição 
+  fflush(stdout);
+  serialPutchar(serial_port, code_req);
+  serialPutchar(serial_port, sensor_addr);
 
-  while (1)
-  {
+  // comando de resposta
+  if (code_req != 3) {  // solicita dados do sensor
 
-    if (serialDataAvail(serial_port))
-    {
-      // dat = serialGetchar (serial_port);        /* receive character serially*/
-      // printf ("%c", dat) ;
-      fflush(stdout);
-      serialPutchar(serial_port, code_req); // tem que ver como vai enviar o hexa/* transmit character serially on port */
+    if (serialDataAvail(serial_port)) {
+      integral_part = read_serial_int(serial_port);
+      decimal_part = read_serial_int(serial_port);
+      //dat = serialGetchar (serial_port);        /* receive character serially*/
+      printf ("%d,%d", integral_part, decimal_part);
+     
     }
   }
+  else { //solicita status do sensor
+    int status = read_serial_int(serial_port);
+    printf ("Status do sensor: %d\n", status);
+  }
+  return 0;
 }
