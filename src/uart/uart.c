5771a96e5e1d2b1c09cb9d4abf1cc82d0f9f424f
Esther <esthersss09@gmail.com>
2022-05-19 23:36:21 -0300

    add menu

--- src/uart/uart.c
+++ src/uart/uart.c
@@ -1,8 +1,3 @@
- /*
-    UART communication on Raspberry Pi using C (WiringPi Library)
-    http://www.electronicwings.com
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -10,31 +5,55 @@
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
-int main ()
+int main()
 {
-  int serial_port ;
+  int serial_port;
+  int req, code_req;
   char dat;
-  if ((serial_port = serialOpen("/dev/ttyAMA0", 9600)) < 0)    /* open serial port */
+  printf("-------------------------------------\n");
+  printf("-----------------MENU----------------\n");
+  printf("-------------------------------------\n");
+  printf("1 - Status do Sensor.\n");
+  printf("2 - Temperatura.\n");
+  printf("3 - Umidade.\n");
+  scanf("%d", &req);
+
+  switch (req)
   {
-    fprintf (stderr, "Unable to open serial device: %s\n", strerror(errno)) ;
-    return 1 ;
+  case 1:
+    code_req = 3;
+    break;
+  case 2:
+    code_req = 4;
+    break;
+  case 3:
+    code_req = 5;
+    break;
+  default:
+    printf("Inválido");
+    break;
+  }
+  if ((serial_port = serialOpen("/dev/ttyAMA0", 9600)) < 0) /* open serial port */
+  {
+    fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
+    return 1;
   }
 
-  if (wiringPiSetup () == -1)                    /* initializes wiringPi setup */
+  if (wiringPiSetup() == -1) /* initializes wiringPi setup */
   {
-    fprintf (stdout, "Unable to start wiringPi: %s\n", strerror(errno)) ;
-    return 1 ;
+    fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));
+    return 1;
   }
 
-  while(1){
-      
-    if(serialDataAvail (serial_port) )
-    { 
-        dat = serialGetchar (serial_port);        /* receive character serially*/    
-        printf ("%c", dat) ;
-        fflush (stdout) ;
-        serialPutchar(serial_port, dat);        /* transmit character serially on port */
-          }
+  while (1)
+  {
+
+    if (serialDataAvail(serial_port))
+    {
+      // dat = serialGetchar (serial_port);        /* receive character serially*/
+      // printf ("%c", dat) ;
+      fflush(stdout);
+      serialPutchar(serial_port, code_req); // tem que ver como vai enviar o hexa/* transmit character serially on port */
     }
-
+  }
 }
