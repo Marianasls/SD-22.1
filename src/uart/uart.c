bd81e9c2873e3f9775cce12eb88509a54b07b008
marianasls <marianalima0803@gmail.com>
2022-05-25 00:51:00 -0300

    update uart rasp

--- src/uart/uart.c
+++ src/uart/uart.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
-
+#include <time.h>
+#include <unistd.h>
 #include <wiringPi.h>
 #include <wiringSerial.h>
 #include <termios.h>
@@ -9,76 +10,64 @@
 #define ADDR "/dev/ttyAMA0"
 #define BAUD_RATE 9600
 
+int read_int(int serial_port){
+  char c = '';
+  read(serial_port, c, sizeof(c));
+  return c;
+}
 struct termios options ;
 int init_wiringSerial() {
+  int serial_port;
   if ((serial_port = serialOpen(ADDR, BAUD_RATE)) < 0) /* open serial port */
   {
     fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
     return 1;
   }
-  tcgetattr (fd, &options) ;   // Read current options
+  tcgetattr(serial_port, &options);   // Read current options
   // enable receiver, set 8 bit data, ignore control lines   
   options.c_cflag |= (CLOCAL | CREAD | CS8);    
   // disable parity generation and 2 stop bits   
   options.c_cflag &= ~(PARENB | CSTOPB);  
   // set the new port options   
-  tcsetattr(fd, TCSANOW, &options);      
-  return fd; 
+  tcsetattr(serial_port, TCSANOW, &options);      
+  return serial_port; 
 }
-/**
- * Leitura serial de inteiro
- * @param fd 
- * @return int 
- */
-int read_serial_int (int fd) {     
-  char ascii_int[8] = {0};     
-  char c = NULL;     
-  int i = 0;     
-
-  read(fd, &c, 1);         
-  //while (c != '\n')     
-  while (c != ' ')  {         
-    ascii_int[i++] = c;         
-    read(fd, &c, 1);     
-  }     
-  return atoi(ascii_int); 
-} 
 
 int main() {
   int serial_port;
   int req=0;
   int code_req, sensor_addr;
-  int integral_part, decimal_part;
-
+  int integral_part, decimal_part, comando_r;
+  printf("-------------------------------------\n");
+  printf("-----------------MENU----------------\n");
+  printf("-------------------------------------\n");
+  printf("1 - Status do Sensor.\n");
+  printf("2 - Temperatura.\n");
+  printf("3 - Umidade.\n");
+  
   while(!req) {
-    printf("-------------------------------------\n");
-    printf("-----------------MENU----------------\n");
-    printf("-------------------------------------\n");
-    printf("1 - Status do Sensor.\n");
-    printf("2 - Temperatura.\n");
-    printf("3 - Umidade.\n");
     scanf("%d", &req);
-     switch (req) {
-      case 1:
-        code_req = 3;
-        break;
-      case 2:
-        code_req = 4;
-        break;
-      case 3:
-        code_req = 5;
-        break;
-      default:
-        printf("Inválido\n");
-        system("clear");
-        break;
+    switch (req) {
+    case 1:
+      code_req = 3;
+      break;
+    case 2:
+      code_req = 4;
+      break;
+    case 3:
+      code_req = 5;
+      break;
+    default:
+      printf("OpÃ§Ã£o invalida, tente novamente.\n");
+      break;
     }
   }
-  printf("Endereço do sensor (De 0 a 31): ");
+
+  printf("EndereÃ§o do sensor (De 0 a 31): ");
   scanf("%d", &sensor_addr);
 
   // controle de porta serial
-  int serial_port = init_wiringSerial();
+  serial_port = init_wiringSerial();
 
   if (wiringPiSetup() == -1) /* inicializa wiringPi setup */
   {
@@ -86,49 +75,66 @@ int main() {
     return 1;
   }
 
-  // comando de requisição 
+  // comando de requisiÃ§Ã£o 
   fflush(stdout);
+  //char addr[] = { sensor_addr };
+  //write(serial_port, addr, sizeof(addr));
+  //char code[] = { code_req };
+  //write(serial_port, code, sizeof(code));
   serialPutchar(serial_port, sensor_addr);
-  fflush(stdout);
   serialPutchar(serial_port, code_req);
-  printf("requisição enviada\n");   
+  printf("requisiÃ§Ã£o enviada\n");   
 
-  // comando de resposta
-  while(1) {
-    if (serialDataAvail(serial_port)) {
-      printf("pronto para recebimento de dados.\n");
-      if (code_req != 3)   // solicita dados do sensor
-      {
-        //integral_part = read_serial_int(serial_port);
-        //decimal_part = read_serial_int(serial_port);
-        char comando_r = serialGetchar(serial_port);
-        integral_part = serialGetchar(serial_port);
-        decimal_part = serialGetchar(serial_port);
-        printf("%c \n", comando_r);
-        if (code_req == 4) 
-      if (code_req == 4) 
-        if (code_req == 4) 
-          printf("Temperatura: ");
-        else
-      else 
-        else
-          printf("Umidade: ");
-        printf ("%d,%d\n", integral_part, decimal_part);
-        
-        if(integral_part) break;
+  if (code_req != 3) { // solicita dados do sensor    
+    while(1) { 
+      if (serialDataAvail(serial_port)) { // comando  
+        printf("pronto para recebimento do comando.\n");       
+        //comando_r = read_int(serial_port);       
+        comando_r = serialGetchar(serial_port);       
+        printf("%d\n", comando_r);   
+        if(comando_r != 1 && comando_r != 2 && comando_r != 47 )
+          return 0;     
+        break;
       }
-      else { //solicita status do sensor
-        //int status = read_serial_int(serial_port);
+    }
+    while(1) {
+      if (serialDataAvail(serial_port)) { // integral 
+        printf("pronto para recebimento do dado.\n"); 
+        //integral_part = read_int(serial_port); 
+        integral_part = serialGetchar(serial_port);   
+        if (comando_r == 2)       
+          printf("Temperatura: ");        
+        else if(comando_r == 1)         
+          printf("Umidade: ");        
+        else
+          printf("comando invalido\n");
+        
+        printf("%d,", integral_part);
+        break;
+      }
+    }
+    while(1) {      
+      if (serialDataAvail(serial_port)) { // decimal          
+        //decimal_part = read_int(serial_port);            
+        decimal_part = serialGetchar(serial_port);            
+        printf("%d\n", decimal_part);
+        break;
+      }
+    }*/
+  } // END IF
+  else{
+    while(1) {
+      if(serialDataAvail(serial_port)) {
+        //int status = read_int(serial_port);
         int status = serialGetchar(serial_port);
-        if(status == 97)
+        if(status == 0)
           printf("Sensor funcionando normalmente.\n", status);
-        else if(status == 98)
+        else if(status == 31)
           printf("Sensor com problema.\n");
-        else printf("recebeu: %c\n", status);
-        
-        if(status) break;       
+        else 
+          printf("status recebeu: %d\n", status);
+        break;
       }
-      
     }
   }
   return 0;
